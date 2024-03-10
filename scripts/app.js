d9c99af0a32fa492fe34a76ae642928356450b48
nuggz <cmst010@gmail.com>
2024-03-10 17:19:27 -0600

    Final image search app

---
+++ scripts/app.js
@@ -0,0 +1,36 @@
+const searchInput = document.getElementById("search-input");
+const searchButton = document.getElementById("search-button");
+const results = document.getElementById("results");
+
+let accessKey = "L65umU1RK4b-QSh_aRvJhEgqiyH1o-4VdjepFvTQhVk";
+
+const fetchImages = async (query) => {
+    const response = await fetch(
+        `https://api.unsplash.com/search/photos?query=${query}&client_id=${accessKey}`
+    );
+    const data = await response.json();
+    clearResults();
+    data.results.forEach((image) => {
+        addImage(image.urls.regular);
+    });
+    return data;
+};
+
+const addImage = (src, alt) => {
+    const resultsContainer = document.getElementById("results");
+    const img = document.createElement("img");
+    img.classList.add("box");
+
+    img.src = src;
+    img.alt = alt;
+
+    resultsContainer.appendChild(img);
+};
+
+const clearResults = () => {
+    results.innerHTML = "";
+};
+
+searchButton.addEventListener("click", () => {
+    fetchImages(searchInput.value);
+});
